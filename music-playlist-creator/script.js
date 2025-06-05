504b0f5e47ba05a84ab07094cf2be14444e38e67
Imani Henderson <hendersoni@meta.com>
2025-06-05 14:04:17 -0700

    june 5 commit 2

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,9 +3,9 @@ const data = {
   "playlists": [
     {
       "playlistID": "1234",
-      "playlist_titles": "Playlist One",
+      "playlist_titles": "Motivational Playlist",
       "author": "Imani",
-      "playlist_images": "assets/img/playlist1.png",
+      "playlist_images": "https://images.unsplash.com/photo-1504805572947-34fad45aed93?auto=format&fit=crop&w=800&q=80",
       "songs": [
         "Song 1",
         "Song 2",
@@ -29,7 +29,7 @@ const data = {
       "playlistID": "5678",
       "playlist_titles": "Summer Vibes",
       "author": "Jaden",
-      "playlist_images": "assets/img/playlist2.png",
+      "playlist_images": "https://cdn.pixabay.com/photo/2022/10/02/10/55/sunflower-7493240_1280.jpg",
       "songs": [
         "Song 1",
         "Song 2",
@@ -53,7 +53,7 @@ const data = {
       "playlistID": "9012",
       "playlist_titles": "Workout Mix",
       "author": "Samantha",
-      "playlist_images": "assets/img/playlist3.png",
+      "playlist_images": "https://images.pexels.com/photos/1464624/pexels-photo-1464624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       "songs": [
         "Song 1",
         "Song 2",
@@ -77,7 +77,7 @@ const data = {
       "playlistID": "3456",
       "playlist_titles": "Chillout Session",
       "author": "Ethan",
-      "playlist_images": "assets/img/playlist4.png",
+      "playlist_images": "https://cdn.pixabay.com/photo/2022/06/21/21/15/audio-7276511_1280.jpg",
       "songs": [
         "Song 1",
         "Song 2",
@@ -99,9 +99,9 @@ const data = {
     },
     {
       "playlistID": "7890",
-      "playlist_titles": "Party Anthems",
+      "playlist_titles": "Party Mix",
       "author": "Lily",
-      "playlist_images": "assets/img/playlist5.png",
+      "playlist_images": "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?auto=format&fit=crop&w=800&q=80",
       "songs": [
         "Song 1",
         "Song 2",
@@ -125,7 +125,7 @@ const data = {
       "playlistID": "2345",
       "playlist_titles": "Focus Flow",
       "author": "Noah",
-      "playlist_images": "assets/img/playlist6.png",
+      "playlist_images": "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
       "songs": [
         "Song 1",
         "Song 2",
@@ -147,9 +147,9 @@ const data = {
     },
     {
       "playlistID": "6789",
-      "playlist_titles": "Retro Revival",
+      "playlist_titles": "Retro Mix",
       "author": "Ava",
-      "playlist_images": "assets/img/playlist7.png",
+      "playlist_images": "https://cdn.pixabay.com/photo/2016/11/23/00/23/hands-1851426_1280.jpg",
       "songs": [
         "Song 1",
         "Song 2",
@@ -173,7 +173,7 @@ const data = {
       "playlistID": "4567",
       "playlist_titles": "Lo-Fi Beats",
       "author": "Julian",
-      "playlist_images": "assets/img/playlist8.png",
+      "playlist_images": "https://cdn.pixabay.com/photo/2024/11/07/10/27/record-shop-9180482_1280.jpg",
       "songs": [
         "Song 1",
         "Song 2",
@@ -207,7 +207,13 @@ for (let i = 0; i < data.playlists.length; i++) {
     <img src="${playlist.playlist_images}" alt="${playlist.playlist_titles}" class="playlist-images">
     <h3 class="playlist-titles">${playlist.playlist_titles}</h3>
     <p class="author">${playlist.author}</p>
-    <p class="like-count">${playlist.like_count}</p>
+
+    <div class="like-container">
+    <button class="like-button">
+        <img class="like-icon" src="https://openclipart.org/image/800px/234835" />
+    </button>
+    <span class="like-count">${playlist.like_count}</span>
+    </div>
     `;
 
     container.appendChild(card);
@@ -252,6 +258,29 @@ document.addEventListener("DOMContentLoaded", function() {
         openModal(playlist);
     });
   });
+});
+
+
+container.querySelectorAll('.like-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const icon = button.querySelector('.like-icon');
+        const countElem = button.nextElementSibling; // .like-count span
+        let count = parseInt(countElem.textContent);
+
+        if (button.classList.contains('liked')) {
+            // Unlike
+            button.classList.remove('liked');
+            icon.src = 'https://openclipart.org/image/800px/234835'; // Use your empty heart icon path
+            count--;
+        } else {
+            // Like
+            button.classList.add('liked');
+            icon.src = 'https://cdn.pixabay.com/photo/2013/07/13/10/50/heart-157895_960_720.png'; // Use your filled heart icon path or inline SVG data URI
+            count++;
+        }
+
+        countElem.textContent = count;
+    });
 });
 
 
